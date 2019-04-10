a69cebe47e1c0f097bd96eac3b198df29322fded
alulab14 <alulab14@inf.pucp.edu.pe>
2019-04-10 16:42:31 -0500

    Creado proyecto en NetBeans 8.0.2.

---
+++ LP 2/LAB 2/INFOSiS/src/pe/edu/pucp/all/model/beam/User.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.all.model.beam;
+
+/**
+ *
+ * @author alulab14
+ */
+public class User {
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+    private int ID;
+    private String Role;
+}
