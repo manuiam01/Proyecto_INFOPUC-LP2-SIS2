b9006addc4b85103cc03f8e999a194aa0e46055e
alulab14 <alulab14@inf.pucp.edu.pe>
2019-05-04 12:15:35 -0500

    Creados los proyectos en NetBeans 8.2

--- LP 2/LAB 4/INFOSiS/src/infosis/INFOSiS.java
+++ LP 2/LAB 4/INFOSiS/src/infosis/INFOSiS.java
@@ -15,8 +15,7 @@ public class INFOSiS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Jajajajajaj lol xd");
-        //xd xd xd xd 
+        
     }
     
 }
