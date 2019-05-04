076e6a797a922cbea1775cccc522253076978aea
alulab14 <alulab14@inf.pucp.edu.pe>
2019-05-04 12:20:30 -0500

    Resolución de conflictos

--- LP 2/LAB 4/INFOSiS/src/infosis/INFOSiS.java
+++ LP 2/LAB 4/INFOSiS/src/infosis/INFOSiS.java
@@ -15,13 +15,10 @@ public class INFOSiS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
         int z=3;
         z=4*3;
-=======
         System.out.println("No hemos hecho nada UwUr");
         int a=2+2;
->>>>>>> 8f8ad772bb00a486efcd4bb2d75c44b57ac489ff
     }
     
 }
