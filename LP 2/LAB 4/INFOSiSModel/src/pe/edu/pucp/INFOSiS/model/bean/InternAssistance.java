5ef0c7777777e1f2d4f2f3b5f09887130a914541
alulab14 <alulab14@inf.pucp.edu.pe>
2019-05-09 21:31:11 -0500

    lp2

--- LP 2/LAB 4/INFOSiSModel/src/pe/edu/pucp/INFOSiS/model/bean/InternAssistance.java
+++ LP 2/LAB 4/INFOSiSModel/src/pe/edu/pucp/INFOSiS/model/bean/InternAssistance.java
@@ -1,4 +1,5 @@
-
+package pe.edu.pucp.INFOSiS.model.bean;
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -32,5 +33,37 @@ public class InternAssistance {
      * 	System.out.println(dtf.format(now)); //2016/11/16 12:08:43
      */
     private LocalDateTime end;
+
+    public InternAssistance(int id, LocalDateTime begin, LocalDateTime end) {
+        this.id = id;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getBegin() {
+        return begin;
+    }
+
+    public void setBegin(LocalDateTime begin) {
+        this.begin = begin;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+    
+    
 
 }
---
+++ LP 2/LAB 4/INFOSiSModel/src/pe/edu/pucp/INFOSiS/model/bean/Professor.java
@@ -0,0 +1,106 @@
+package pe.edu.pucp.INFOSiS.model.bean;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Professor extends Person {
+
+    /**
+     * Default constructor
+     */
+    public Professor() {
+    }
+
+    /**
+     * 
+     */
+    private String idPUCP;
+
+    /**
+     * 
+     */
+    private String emailPUCP;
+
+    /**
+     * 
+     */
+    private Date birthDate;
+
+    /**
+     * 
+     */
+    private ArrayList<Course> coursesCanTeach;
+
+    /**
+     * 
+     */
+    private boolean isActive;
+
+    /**
+     * 
+     */
+    private ArrayList<RoleHistory> history;
+
+    public Professor(String idPUCP, String emailPUCP, Date birthDate, ArrayList<Course> coursesCanTeach, boolean isActive, ArrayList<RoleHistory> history) {
+        this.idPUCP = idPUCP;
+        this.emailPUCP = emailPUCP;
+        this.birthDate = birthDate;
+        this.coursesCanTeach = coursesCanTeach;
+        this.isActive = isActive;
+        this.history = history;
+    }
+
+    public String getIdPUCP() {
+        return idPUCP;
+    }
+
+    public void setIdPUCP(String idPUCP) {
+        this.idPUCP = idPUCP;
+    }
+
+    public String getEmailPUCP() {
+        return emailPUCP;
+    }
+
+    public void setEmailPUCP(String emailPUCP) {
+        this.emailPUCP = emailPUCP;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public ArrayList<Course> getCoursesCanTeach() {
+        return coursesCanTeach;
+    }
+
+    public void setCoursesCanTeach(ArrayList<Course> coursesCanTeach) {
+        this.coursesCanTeach = coursesCanTeach;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public ArrayList<RoleHistory> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<RoleHistory> history) {
+        this.history = history;
+    }
+
+
+
+
+
+}
